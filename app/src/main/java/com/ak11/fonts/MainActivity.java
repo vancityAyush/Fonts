2f36038b3584763f65243488a42ebf79b2e95738
vancityAyush <eliasakeleven@gmail.com>
2021-06-19 05:26:58 +0530

    Initial

---
+++ app/src/main/java/com/ak11/fonts/MainActivity.java
@@ -0,0 +1,45 @@
+package com.ak11.fonts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Typeface font2 = Typeface.createFromAsset(getAssets(),"fonts/fontFofb.ttf");
+        Typeface font3 = Typeface.createFromAsset(getAssets(),"fonts/fontSummer.ttf");
+        Typeface font1 = Typeface.createFromAsset(getAssets(),"fonts/font1.ttf");
+
+        TextView txtView = findViewById(R.id.textView);
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+
+                    case R.id.radioButton1:
+                        txtView.setTypeface(font1);
+                        break;
+                    case R.id.radioButton2:
+                        txtView.setTypeface(font2);
+                        break;
+                    case R.id.radioButton3:
+                        txtView.setTypeface(font3);
+
+                }
+            }
+        });
+
+
+    }
+}
